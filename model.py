7cc735b04fa0708b09b9300a09345c69cfe91be0
Emmett McFarlane <StaminaCode@Gmail.com>
2019-11-15 17:20:00 -0500

    Made it actually work

--- model.py
+++ model.py
@@ -1,56 +1,84 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import kde
+
+m = 1
+A, B = 1, 1 #1e6, 1e7
 
 # Calculates the force exerted on particle i
 def lennard_jones_force(i):
-    global xs
-    sigma = 0.1
-    epsilon = 1
+    global A, B, xs
     F_net = np.zeros(2)
     for j in range(len(xs)):
         if (i != j):
-            r = np.sqrt(np.sum((xs[j] - xs[i])**2)) # Calculate distance
-            R = (xs[j] - xs[i]) / r # Normalized direction vector
-            F = -24*pow(sigma, 6)*epsilon*(pow(r, 6) - 2*pow(sigma, 6)) / (pow(r, 13) + 1e-10)
+            r = np.sqrt(np.sum((xs[j] - xs[i])**2))# Distance
+            R = -(xs[j] - xs[i]) / r # Normalized direction vector
+            F = 12*A/pow(r, 13) - 6*B/pow(r, 7) # Force between i and j
             F_net += F*R
     return F_net
 
 # verlet integrates particle i
-# TODO: Make this leapfrog instead
 def verlet_integrate(i):
-    global xs
-    global xs_prev
-    dt = 0.1
-    xs_new = xs
-    a = lennard_jones_force(i)
-    xs_new[i] = 2*xs[i] - xs_prev[i] + a*dt**2
-    xs_prev = xs
-    xs = xs_new
+    global xs, vs, dt, m
+    a = (1/m) * lennard_jones_force(i)
+    vs[i] += a * dt
+    xs[i] += vs[i] * dt
 
 # Initializes a model with n particles
 def initialize(n):
     # Initialize pyplot stuff
-    global fig, ax
+    global fig, ax, box_size
+    box_size = 10
+    #plt.xlim(-2, 200)
+    #plt.ylim(-2, 20)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.show()
-    fig.canvas.draw()
-    # Initialize particle positions
-    global xs, xs_prev
-    xs = np.random.rand(n, 2)
-    xs_prev = xs
+
+    # Set particle positions on a grid
+    global xs, vs
+    #x = np.arange(-(box_size/2), (box_size/2), box_size/np.sqrt(n))
+    #y = np.arange(-(box_size/2), (box_size/2), box_size/np.sqrt(n))
+    #X,Y = np.meshgrid(x,y)
+    #xs = np.array([X.flatten(),Y.flatten()]).T + np.random.rand(n, 2)*3
+    #vs = np.random.rand(n, 2) * 0
+    xs = (np.random.rand(n, 2) - 0.5) * box_size
+    vs = (np.random.rand(n, 2) - 0.5)
 
 def observe():
-    global xs, fig, ax
+    global xs, fig, ax, t
     ax.clear()
-    ax.scatter(xs[:, 0], xs[:, 1])
+    plt.xlim(-box_size, box_size)
+    plt.ylim(-box_size, box_size)
+    #ax.set_facecolor((0.1, 0.1, 0.1))
+    ax.scatter(xs[:, 0], xs[:, 1]) #, color = (0.7, 0.7, 0.7))
     fig.canvas.draw()
+    # ax.axes.get_xaxis().set_visible(False)
+    # ax.axes.get_yaxis().set_visible(False)
+    # plt.savefig('figures/' + str(t) + '.png', bbox_inches='tight', pad_inches = -0.06)
 
 def update():
     for i in range(len(xs)):
         verlet_integrate(i)
 
-initialize(n = 50)
-for t in range(1000):
+def compute_total_energy():
+    global xs, vs, A, B, g
+    total_potential = 0
+    total_kinetic = 0
+    for i in range(len(xs)):
+        v = np.sqrt(np.sum(vs[i]**2))
+        KE = m * v * v / 2
+        total_kinetic += KE
+        for j in range(len(xs)):
+            if (i != j):
+                r = np.sqrt(np.sum((xs[j] - xs[i])**2))
+                V = (-A/pow(r, 12)) + (B/pow(r, 6))
+                total_potential += V
+    return (total_potential/2 + total_kinetic)
+
+dt = 0.01
+initialize(n = 4)
+for t in range(10000):
     observe()
     update()
+    print(compute_total_energy()
